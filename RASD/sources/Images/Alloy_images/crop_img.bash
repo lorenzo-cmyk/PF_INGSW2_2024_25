494379eb42da4be926ee74ed4731fd4cf5aae913
Giacomo Bossi <161643303+Giacomo-Bossi@users.noreply.github.com>
2024-12-11 17:55:06 +0000

    [Alloy] cropped images for better readability

---
+++ RASD/sources/Images/Alloy_images/crop_img.bash
@@ -0,0 +1,19 @@
+start=1
+end=5
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <number>"
+  exit 1
+fi
+
+if ! [[ "$1" =~ ^-?[0-9]+$ ]]; then
+  echo "Error: Argument must be a valid number."
+  exit 1
+fi
+
+px=$1
+
+for i in $(seq $start $end)
+do
+    pdfcrop --margins "0 0 -${px} 0" Alloy_1-${i}.pdf Alloy_1-${i}.pdf
+done
